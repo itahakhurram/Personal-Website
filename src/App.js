3106dd3d63e657b4272d5abfe595b17a98a93f14
itahakhurram <taha10@hotmail.co.uk>
2021-04-01 20:37:56 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import MyNavbar from "./components/my-navbar/mynavbar.component";
+import mynavbar from "./components/my-navbar/mynavbar.component";
+
+const App = () => {
+  return  (<div>
+    <MyNavbar />
+    My Protfolio</div>
+
+  );
+};
+
+export default App;
